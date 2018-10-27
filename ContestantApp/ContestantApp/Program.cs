084fb55b8390527b2a77b54063599778914da463
Felix Vigneault <minivigno@gmail.com>
2018-10-27 09:56:47 -0400

    Added base code

---
+++ ContestantApp/ContestantApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using ContestantApp.Utilities;
+using SolutionViewer.Model;
+using SolutionViewer.Utilities;
+
+namespace ContestantApp
+{
+  internal class Program
+  {
+    private static void Main()
+    {
+      const String TeamId = "5bd1e020ce65420001eccb91";
+
+      Validate( TeamId, GetPoints() );
+    }
+
+    private static List<Point> GetPoints()
+    {
+      return new List<Point>();
+    }
+
+    private static void Validate( String _teamId, List<Point> _points )
+    {
+      PointManager pointManager = new PointManager();
+
+      TaskUtilities.GetSTATask( () =>
+      {
+        Double score;
+        Boolean success = pointManager.SetSolutionPoints( _teamId, _points, out score, out String errorMsg );
+
+        if ( success )
+        {
+          Console.WriteLine( $@"Score: {score}" );
+        }
+        else
+        {
+          Console.WriteLine( $@"Message: {errorMsg}" );
+        }
+      } );
+
+      pointManager.WriteSolutionToFile( _teamId + ".json", _points );
+    }
+  }
+}
---
+++ ContestantApp/ContestantApp/Utilities/TaskUtilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ContestantApp.Utilities
+{
+  public class TaskUtilities
+  {
+    public static Task GetSTATask( Action _action )
+    {
+      TaskCompletionSource<Object> source = new TaskCompletionSource<Object>();
+
+      Thread thread = new Thread( () =>
+      {
+        try
+        {
+          _action();
+          source.SetResult( null );
+        }
+        catch ( Exception ex )
+        {
+          source.SetException( ex );
+        }
+      } );
+
+      thread.SetApartmentState( ApartmentState.STA );
+      thread.Start();
+
+      return source.Task;
+    }
+  }
+}
