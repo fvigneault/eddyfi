5c8ff0fa19665ed1e3e256fd13753bd7feb80ff7
Felix Vigneault <minivigno@gmail.com>
2018-10-27 13:41:51 -0400

    Complex version

--- ContestantApp/ContestantApp/Program.cs
+++ ContestantApp/ContestantApp/Program.cs
@@ -26,13 +26,108 @@ namespace ContestantApp
         return JsonConvert.DeserializeObject<List<Point>>(json);
       }
     }
-
-    private enum Direction
+    
+    class Link
     {
-      Up,
-      Down,
-      Left,
-      Right
+      public Point p1;
+      public Point p2;
+      public double distance;
+    }
+
+    private static List<Link> GetAllLinks(List<Point> points)
+    {
+      var links = new List<Link>();
+      for (int i = 0; i < points.Count; ++i)
+      {
+        for (int j = i + 1; j < points.Count; ++j)
+        {
+          links.Add(new Link
+          {
+            p1 = points[i],
+            p2 = points[j],
+            distance = GetDistance(points[i], points[j])
+          });
+        }
+      }
+      links.Sort((l1, l2) => l1.distance.CompareTo(l2.distance));
+      return links;
+    }
+
+    private static List<Link> GetSubListLinks(List<Link> links)
+    {
+      List<Link> optimalLinks = new List<Link>();
+      Dictionary<Point, int> timesPointIsUsed = new Dictionary<Point, int>();
+
+      foreach (Link currentLink in links)
+      {
+        var point1CanBeUsed = !timesPointIsUsed.ContainsKey(currentLink.p1) || timesPointIsUsed[currentLink.p1] == 1;
+        var point2CanBeUsed = !timesPointIsUsed.ContainsKey(currentLink.p2) || timesPointIsUsed[currentLink.p2] == 1;
+
+        if (point1CanBeUsed && point2CanBeUsed)
+        {
+          optimalLinks.Add(currentLink);
+
+          if (!timesPointIsUsed.ContainsKey(currentLink.p1))
+          {
+            timesPointIsUsed[currentLink.p1] = 0;
+          }
+          if (!timesPointIsUsed.ContainsKey(currentLink.p2))
+          {
+            timesPointIsUsed[currentLink.p2] = 0;
+          }
+          timesPointIsUsed[currentLink.p1] = timesPointIsUsed[currentLink.p1] + 1;
+          timesPointIsUsed[currentLink.p2] = timesPointIsUsed[currentLink.p2] + 1;
+        }
+      }
+      return optimalLinks;
+    }
+
+    private static List<Point> GetPathFromLinks(List<Link> links)
+    {
+      var path = new List<Point>();
+
+      var firstLink = links.First();
+      path.Add(firstLink.p1);
+      path.Add(firstLink.p2);
+      links.Remove(firstLink);
+
+      while (links.Count != 0)
+      {
+        var currentLink = links.First();
+        links.RemoveRange(0, 1);
+
+        var indexP1 = path.IndexOf(currentLink.p1);
+        var indexP2 = path.IndexOf(currentLink.p2);
+
+        if (indexP1 == -1 && indexP2 == -1)
+        {
+          links.Add(currentLink);
+        }
+        else if (indexP1 != -1 && indexP2 == -1)
+        {
+          if (indexP1 + 1 == path.Count)
+          {
+            path.Add(currentLink.p2);
+          }
+          else
+          {
+            path.Insert(indexP1 + 1, currentLink.p2);
+          }
+        }
+        else if (indexP1 == -1 && indexP2 != -1)
+        {
+          path.Insert(indexP2, currentLink.p1);
+        }
+        else
+        {
+          var a = 2;
+        }
+      }
+
+      var indexOfFirstPoint = path.FindIndex(point => point.Value == 0);
+      var bestPath = path.Skip(indexOfFirstPoint).ToList();
+      bestPath.AddRange(path.Take(indexOfFirstPoint).ToList());
+      return bestPath;
     }
 
     private static List<Point> GetPoints()
@@ -40,6 +135,13 @@ namespace ContestantApp
       List<Point> map = LoadMap();
 
       map.Sort((point1, point2) => point1.Value.CompareTo(point2.Value));
+
+      var payingMapWithFirst = new List<Point>(map);
+      payingMapWithFirst.RemoveRange(1, 30);
+      var allLinksPossible = GetAllLinks(payingMapWithFirst);
+      var optimalLinks = GetSubListLinks(allLinksPossible);
+      var bestPath = GetPathFromLinks(optimalLinks);
+      return bestPath;
 
       var payingMap = map.Skip(31).ToList();
 
